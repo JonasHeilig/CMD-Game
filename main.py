aea5bb3545f84be0f190a2b3ef530eba65fbd081
Jonas Heilig <jonas.maximilian@die-heiligs.de>
2024-06-20 16:48:22 +0200

    Start Game Routine Create New Player Login To Player

--- main.py
+++ main.py
@@ -1,8 +1,61 @@
-import log_system
+import os
+import json
 
-Logger = log_system.Logger()
-Logger.start_log()
 
-Logger.log_info(message="Test")
+def start_game():
+    print("Welcome Player!")
+    if os.path.exists('user'):
+        if any(fname.endswith('.json') for fname in os.listdir('user')):
+            choice = input("New Game (n) or Return to Game (r)? ").strip().lower()
+        else:
+            choice = 'n'
+    else:
+        os.makedirs('user')
+        choice = 'n'
 
-Logger.log_close_log()
+    if choice == 'n':
+        new_game()
+    elif choice == 'r':
+        continue_game()
+    else:
+        print("Incorrect Input")
+        start_game()
+
+
+def new_game():
+    name = input("Enter your Player Name: ").strip()
+    filename = f'user/{name}.json'
+    if os.path.exists(filename):
+        print(f"A Player with the name '{name}' already exist. Take an other name.")
+        new_game()
+    else:
+        player_data = {
+            'name': name,
+            'level': 1,
+            'score': 0
+        }
+        with open(filename, 'w') as file:
+            json.dump(player_data, file)
+        print(f"New Game created for {name}!")
+        game_loop(player_data)
+
+
+def continue_game():
+    name = input("Enter your Player Name: ").strip()
+    filename = f'user/{name}.json'
+    if os.path.exists(filename):
+        with open(filename, 'r') as file:
+            player_data = json.load(file)
+        print(f"Welcome Back, {name}!")
+        game_loop(player_data)
+    else:
+        print(f"No Player with the name '{name}' found.")
+        start_game()
+
+
+def game_loop(player_data):
+    print(player_data)
+
+
+if __name__ == "__main__":
+    start_game()
